44a4415c6624b52c0c19c5263873290c162bb333
shravanilongview <shravani.longview@gmail.com>
2020-09-23 12:52:38 +0530

    minor changes in search frontend

--- lib/SearchAdvisor.dart
+++ lib/SearchAdvisor.dart
@@ -1,11 +1,8 @@
-
 import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'advisor_data.dart';
 
 class SearchAdvisorPage extends StatefulWidget {
@@ -17,21 +14,18 @@ class _SearchAdvisorPage extends State<SearchAdvisorPage> {
   List searchList = [];
   Future userAdvisorData() async {
     var url = 'http://sanjayagarwal.in/Finance App/SearchAdvisor.php';
-    final response = await http.post(
-        url
-    );
-    if(response.statusCode == 200)
-    {
+    final response = await http.post(url);
+    if (response.statusCode == 200) {
       var jsonData = jsonDecode(response.body);
 
-      for(var i =0; i<jsonData.length; i++)
-      {
+      for (var i = 0; i < jsonData.length; i++) {
         searchList.add(jsonData[i]['Name']);
       }
 
       print(searchList);
     }
   }
+
   @override
   void initState() {
     super.initState();
@@ -42,13 +36,6 @@ class _SearchAdvisorPage extends State<SearchAdvisorPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
-        title: Text('Search Advisor' , style: TextStyle(color: Colors.black),),
-        actions:<Widget> [
-          IconButton(onPressed: () {
-            showSearch(context: context, delegate: AdvisorSearch(list: searchList));
-            }, icon: Icon(Icons.search),)
-
         leading: IconButton(
           onPressed: () {
             Navigator.pop(context);
@@ -56,22 +43,26 @@ class _SearchAdvisorPage extends State<SearchAdvisorPage> {
           icon: Icon(Icons.arrow_back_ios),
           color: Color(0xff373D3F),
         ),
+        backgroundColor: Color(0xff63E2E0),
+        centerTitle: true,
         title: Text(
           'SEARCH ADVISOR',
-          style: TextStyle(color: Colors.black),
+          style: TextStyle(
+            color: Color(0xff373D3F),
+          ),
         ),
-        centerTitle: true,
         actions: <Widget>[
           IconButton(
             onPressed: () {
-              showSearch(context: context, delegate: AdvisorSearch());
+              showSearch(
+                  context: context, delegate: AdvisorSearch(list: searchList));
             },
-            icon: Icon(Icons.search),
-            color: Color(0xff373D3F),
+            icon: Icon(
+              Icons.search,
+              color: Color(0xff373D3F),
+            ),
           )
-
         ],
-        backgroundColor: Color(0xff63E2E0),
       ),
     );
   }
@@ -90,26 +81,23 @@ class AdvisorSearch extends SearchDelegate<AdvisorData> {
     );
     if (response.statusCode == 200) {
       var jsonData = jsonDecode(response.body);
-      print("**********************************************************************");
+      print(
+          "**********************************************************************");
       print(query);
-      print("**********************************************************************");
+      print(
+          "**********************************************************************");
       return jsonData;
     }
   }
 
   @override
   List<Widget> buildActions(BuildContext context) {
-    return [IconButton(icon: Icon(Icons.search), onPressed: () {
-      query = "";
-      showSuggestions(context);
-    },)];
-  @override
-  List<Widget> buildActions(BuildContext context) {
     return [
       IconButton(
-        icon: Icon(Icons.clear),
+        icon: Icon(Icons.search),
         onPressed: () {
           query = "";
+          showSuggestions(context);
         },
       )
     ];
@@ -123,78 +111,49 @@ class AdvisorSearch extends SearchDelegate<AdvisorData> {
       },
       icon: Icon(Icons.arrow_back_ios),
     );
-    return IconButton(
-      onPressed: () {
-        close(context, null);
-      },
-      icon: Icon(Icons.arrow_back),
-    );
   }
 
   @override
   Widget buildResults(BuildContext context) {
-    return FutureBuilder (
+    return FutureBuilder(
       future: advisorData(),
-      builder: (context, snapshot){
-        if(snapshot.hasData){
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
           return ListView.builder(
               itemCount: snapshot.data.length,
-              itemBuilder: (context,index){
+              itemBuilder: (context, index) {
                 var list = snapshot.data[index];
 
-                return ListTile(title: Text(list['AdvisorID']),);
+                return ListTile(
+                  title: Text(list['AdvisorID']),
+                );
               });
         }
         return CircularProgressIndicator();
-      },);
-    return Center(child: Text(query, style: TextStyle(fontSize: 20)));
+      },
+    );
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-
-    var listData =  query.isEmpty ? list : list.where((element) => element.contains(query)).toList();
-    return listData.isEmpty ? Center(child: Text('NO ADVISOR FOUND',  style: TextStyle(fontSize: 20), )) : ListView.builder(
-        itemCount: listData.length,
-        itemBuilder: (context, index){
-          return ListTile(
-            onTap: (){
-              query = listData[index];
-              showResults(context);
-            },
-            title: Text(listData[index]),);
-        });
-    final advisorList = query.isEmpty
-        ? loadAdvisorData()
-        : loadAdvisorData().where((p) => p.UserName.startsWith(query)).toList();
-
-    return advisorList.isEmpty
-        ? Text(
-            'No Advisor Found',
+    var listData = query.isEmpty
+        ? list
+        : list.where((element) => element.contains(query)).toList();
+    return listData.isEmpty
+        ? Center(
+            child: Text(
+            'NO ADVISOR FOUND',
             style: TextStyle(fontSize: 20),
-          )
+          ))
         : ListView.builder(
-            itemCount: advisorList.length,
+            itemCount: listData.length,
             itemBuilder: (context, index) {
-              final AdvisorData listItem = advisorList[index];
               return ListTile(
                 onTap: () {
+                  query = listData[index];
                   showResults(context);
                 },
-                title: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      listItem.UserName,
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    Text(
-                      listItem.UserID,
-                      style: TextStyle(color: Colors.grey),
-                    ),
-                    Divider()
-                  ],
-                ),
+                title: Text(listData[index]),
               );
             });
   }
--- lib/SearchUser.dart
+++ lib/SearchUser.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'package:adminapp/user_data.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -13,20 +12,16 @@ class _SearchUserPage extends State<SearchUserPage> {
   List searchList = [];
   Future userSearchData() async {
     var url = 'http://sanjayagarwal.in/Finance App/SearchUser.php';
-    final response = await http.post(
-      url
-    );
-    if(response.statusCode == 200)
-     {
-       var jsonData = jsonDecode(response.body);
+    final response = await http.post(url);
+    if (response.statusCode == 200) {
+      var jsonData = jsonDecode(response.body);
 
-       for(var i =0; i<jsonData.length; i++)
-         {
-           searchList.add(jsonData[i]['Name']);
-         }
+      for (var i = 0; i < jsonData.length; i++) {
+        searchList.add(jsonData[i]['Name']);
+      }
 
-       print(searchList);
-     }
+      print(searchList);
+    }
   }
 
   @override
@@ -40,14 +35,6 @@ class _SearchUserPage extends State<SearchUserPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
-        title: Text('Search User', style: TextStyle(color: Colors.black),),
-        actions:<Widget> [
-          IconButton(onPressed: () {
-            showSearch(context: context, delegate: UserSearch(list: searchList));
-
-          }, icon: Icon(Icons.search),)
-
         leading: IconButton(
           onPressed: () {
             Navigator.pop(context);
@@ -55,67 +42,63 @@ class _SearchUserPage extends State<SearchUserPage> {
           icon: Icon(Icons.arrow_back_ios),
           color: Color(0xff373D3F),
         ),
+        backgroundColor: Color(0xff63E2E0),
         centerTitle: true,
         title: Text(
           'SEARCH USER',
-          style: TextStyle(color: Colors.black),
+          style: TextStyle(
+            color: Color(0xff373D3F),
+          ),
         ),
         actions: <Widget>[
           IconButton(
             onPressed: () {
-              showSearch(context: context, delegate: UserSearch());
+              showSearch(
+                  context: context, delegate: UserSearch(list: searchList));
             },
-            icon: Icon(Icons.search),
-            color: Color(0xff373D3F),
+            icon: Icon(
+              Icons.search,
+              color: Color(0xff373D3F),
+            ),
           )
         ],
-        backgroundColor: Color(0xff63E2E0),
       ),
     );
   }
 }
 
-class UserSearch extends SearchDelegate<String>{
-
+class UserSearch extends SearchDelegate<String> {
   List<dynamic> list;
   UserSearch({this.list});
 
   Future userData() async {
     var url = 'http://sanjayagarwal.in/Finance App/userData.php';
     final response = await http.post(
-        url,
+      url,
       body: jsonEncode(<String, String>{
         "Name": query,
-
       }),
     );
 
-    if(response.statusCode == 200)
-    {
+    if (response.statusCode == 200) {
       var jsonData = jsonDecode(response.body);
-      print("**********************************************************************");
+      print(
+          "**********************************************************************");
       print(query);
-      print("**********************************************************************");
+      print(
+          "**********************************************************************");
       return jsonData;
-
     }
   }
 
   @override
   List<Widget> buildActions(BuildContext context) {
-
-    return [IconButton(icon: Icon(Icons.search), onPressed: () {
-      query = "";
-      showSuggestions(context);
-    },)];
-class UserSearch extends SearchDelegate<UserData> {
-  @override
-  List<Widget> buildActions(BuildContext context) {
     return [
       IconButton(
-        icon: Icon(Icons.clear),
+        icon: Icon(Icons.search),
         onPressed: () {
           query = "";
+          showSuggestions(context);
         },
       )
     ];
@@ -129,79 +112,49 @@ class UserSearch extends SearchDelegate<UserData> {
       },
       icon: Icon(Icons.arrow_back_ios),
     );
-    return IconButton(
-      onPressed: () {
-        close(context, null);
-      },
-      icon: Icon(Icons.arrow_back),
-    );
   }
 
   @override
   Widget buildResults(BuildContext context) {
-    return FutureBuilder (
+    return FutureBuilder(
       future: userData(),
-      builder: (context, snapshot){
-        if(snapshot.hasData){
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
           return ListView.builder(
-            itemCount: snapshot.data.length,
-              itemBuilder: (context,index){
-              var list = snapshot.data[index];
+              itemCount: snapshot.data.length,
+              itemBuilder: (context, index) {
+                var list = snapshot.data[index];
 
-            return ListTile(title: Text(list['UserID']),);
-          });
+                return ListTile(
+                  title: Text(list['UserID']),
+                );
+              });
         }
         return CircularProgressIndicator();
-        },);
-    return Center(child: Text(query, style: TextStyle(fontSize: 20)));
+      },
+    );
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-   var listData =  query.isEmpty ? list : list.where((element) => element.contains(query)).toList();
-    return listData.isEmpty ? Center(child: Text('NO USER FOUND',  style: TextStyle(fontSize: 20), )) : ListView.builder(
-      itemCount: listData.length,
-        itemBuilder: (context, index){
-          return ListTile(
-            onTap: (){
-              query = listData[index];
-              showResults(context);
-            },
-            title: Text(listData[index]),);
-    });
-
-  }
-    final userList = query.isEmpty
-        ? loadUserData()
-        : loadUserData().where((p) => p.UserName.startsWith(query)).toList();
-
-    return userList.isEmpty
-        ? Text(
-            'No User Found',
+    var listData = query.isEmpty
+        ? list
+        : list.where((element) => element.contains(query)).toList();
+    return listData.isEmpty
+        ? Center(
+            child: Text(
+            'NO USER FOUND',
             style: TextStyle(fontSize: 20),
-          )
+          ))
         : ListView.builder(
-            itemCount: userList.length,
+            itemCount: listData.length,
             itemBuilder: (context, index) {
-              final UserData listItem = userList[index];
               return ListTile(
                 onTap: () {
+                  query = listData[index];
                   showResults(context);
                 },
-                title: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      listItem.UserName,
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    Text(
-                      listItem.UserID,
-                      style: TextStyle(color: Colors.grey),
-                    ),
-                    Divider()
-                  ],
-                ),
+                title: Text(listData[index]),
               );
             });
   }
